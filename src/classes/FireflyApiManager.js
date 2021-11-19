d7a65becbed39445b220d6149912b2787ecd39e8
Franco Silva <57300093+dev-franco@users.noreply.github.com>
2021-11-19 23:43:25 +0000

    Updating apiUrl + fixing 'TRF' case when debit < 0

--- src/classes/FireflyApiManager.js
+++ src/classes/FireflyApiManager.js
@@ -3,11 +3,11 @@ const Transaction = require('./Transaction');
 class FireFlyApiManager {
 
     settings = {
-        apiUrl: "http://192.168.1.90:7000/api/v1",
+        apiUrl: "http://192.168.1.205:7000/api/v1",
         jwtToken : "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5IiwianRpIjoiZWZmMzA5ODBlYzM2ODY4OGI4ZjJkM2Y4ZTE0M2I4YTFmNWM4MTA3MmJkNmY4MzM0ZThlNWQyYWUxN2U1NTQyYWE2MWM3YjZjNjZiNmYzZWQiLCJpYXQiOjE2MzY4MjE3OTMuMTA4MzU3LCJuYmYiOjE2MzY4MjE3OTMuMTA4MzY3LCJleHAiOjE2NjgzNTc3OTMuMDMwNzc1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.VC3prT11OadBaKS8rmbih8n2poKLqxWHonnLUWD306V94F9I5OfkszWVjW7H5Jlu5-X5LQr9uyOdAYPcP66XavBj-mVgGOwSd2xkm5QkjkV9Dzj8qLw65WNvXIfPCwfYx9ikCK-BtQoMy85sawpp7Jpzz6EtuZyfm8dAc-Dvhyz9ln13vzomvgTFGvFwITlAOzv6ijOZMXYaV16FWzuhctMdoJ8CI7u7asONPeJ3GUIBliiHWhkwHpgZAzdhZWWUmkfrfFlIrxWr_TZ2TQn6Z6HnA5BSQCBMaHFKVKawl444iiAH14TnwXveJbyu8HXe3upvLnW-o0insHbFiWe8Ahs-f9wIVPxJ7mpx8hWfteJJxU0mIJpvyNhCl3OA6cB9usQNug2aurbRd_s60YhuOpFDFjfYHrtf5ZOTa-82SyLt04mEZwGZUdQnnpJWGUMnFC35BPiovsN5IHyKd3_2RNALssP7gDWpksHVGFdD9cDlynD88mK8W3jTgWjtOIlq04mPaLOPzxNYKIY0BF37eZk8o_tFzb3XB3QhjdDI1DGpYqunOCN_EuZ5jaJ1U24l6sFmuG2U_KSu2yp9ONHRLrCKtqk_fu0xE4Kv_m-0ksGTUqzcWysppAUD_07MfxRpzk-UsR2PqlI6OSFFDB_C0EvHx-bpuPh-ydSR9B0gYDo",
         mainAccountName: "Novo Banco",
         savingsAccountName: "Novo Banco savings account",
-        connectionTimeout: 10 // in seconds
+        connectionTimeout: 20 // in seconds
     }
 
     /**
@@ -22,6 +22,7 @@ class FireFlyApiManager {
             // we need to split the transactions into types, other wise
             // we will get a 'All splits must be of the same type' error message
             let fireFlyTransactions = this.prepareTransactionsForFirefly(transactions);
+
             // we reverse both arrays because since we have no hours on our dates
             // firefly will think 2 records inserted in the same day means their order was 1 => 2; when in reality it's 2 => 1 (later transaction is higher than previous)
             let fireFlyDebitTransactions = fireFlyTransactions.filter(t => t.type === 'deposit').reverse();
@@ -50,6 +51,7 @@ class FireFlyApiManager {
                         console.log(`Inserted ${withdrawalPayload.transactions.length} transactions of type withdrawal into Firefly`);
                     },
                     (error) => {
+                        console.log('Error on withdrawal payload:');
                         console.log(error.response.data)
                     }
                 );
@@ -61,6 +63,7 @@ class FireFlyApiManager {
                         console.log(`Inserted ${depositPayload.transactions.length} transactions of type deposit into Firefly`);
                     },
                     (error) => {
+                        console.log('Error on deposit payload:');
                         console.log(error.response.data)
                     }
                 );
@@ -109,22 +112,32 @@ class FireFlyApiManager {
                     // transfers for external accounts, like expenses, or deposits
                     // we would be better off by looking into the amount of the transfer and determine if it's deposit or withdrawal
                     case 'transfer':
-                        let isDeposit = (t.credit > 0 && t.debit === 0);
+                        let isDeposit = (t.credit > 0);
                         transaction.amount = isDeposit ? Math.abs(t.credit) : Math.abs(t.debit);
-                        transaction.type = this.transactionTypeToFireflyType(isDeposit ? 'CRD' : 'DEB');
+                        transaction.type = this.transactionTypeToFireflyType('TRF');
 
                         if(isDeposit) {
                             transaction.destination_name = this.settings.mainAccountName;
                             transaction.amount = Math.abs(t.credit);
                         } else {
                             transaction.source_name = this.settings.mainAccountName;
-                            transaction.amount = Math.abs(t.debit);
+                            transaction.amount = Math.abs(parseFloat(t.debit));
                         }
                         break;
                 
                     default:
                         throw(`transaction type case not implemented for: ${transactionType}`)
                         break;
+                }
+
+                if(isNaN(transaction.amount)) {
+                    console.log('This got screwd up somewhere');
+                    console.log(transaction);
+                    console.log('Transcription from:')
+                    console.log(t);
+                    console.log('Is deposit?')
+                    console.log((t.credit > 0))
+                    return;
                 }
 
                 return transaction;
@@ -150,7 +163,7 @@ class FireFlyApiManager {
             'LEV' : 'withdrawal',
         }
         if(transaction.type === 'TRF') {
-            let isDebit = transaction.debit > 0 && transaction.credit === 0;
+            let isDebit = Math.abs(transaction.debit) > 0;
             return isDebit ? types['DEB'] : types['CRD'];
         } else {
             return transaction.type ? types[transaction.type] : null;
@@ -175,11 +188,6 @@ class FireFlyApiManager {
        
         const axios = require('axios');
         let req;
-
-        let timeout = setTimeout(() => {
-            throw(`Connection timed out after ${this.settings.connectionTimeout} seconds. Check your apiUrl configuration`);
-        }, this.settings.connectionTimeout * 1000)
-
         const options = {
             headers: {
                 Authorization: `Bearer ${this.settings.jwtToken}`,
