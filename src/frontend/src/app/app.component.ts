2a3533a7dcd1201333cc626a8ffe315541fa1dcc
Franco Silva <57300093+dev-franco@users.noreply.github.com>
2021-11-28 23:56:17 +0000

    First attempt at frontend uploader

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
   title = 'frontend';
+  apiUrl = 'http://localhost:3000';
+  apiEndpoint = '/firefly/sync/novobanco'
+  http: HttpClient
+
+  constructor(http: HttpClient) {
+   this.http = http;
+  }
+
+  public files: NgxFileDropEntry[] = [];
+
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    for (const droppedFile of files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+
+         const formData = new FormData()
+         formData.append('file', file, droppedFile.relativePath)
+         this.http.post(`${this.apiUrl}${this.apiEndpoint}`, formData, { responseType: 'json' })
+          .subscribe((data: any) => {
+            console.log('Got data from backedn')
+            console.log(data);
+          })
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event: any){
+    console.log(event);
+  }
+
+  public fileLeave(event: any){
+    console.log(event);
+  }
+
 }
